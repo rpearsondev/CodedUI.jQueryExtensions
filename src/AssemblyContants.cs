1cdc352c5d9ecc36387badfe62c89efd004669c7
rpearsondev <rpearsondev@gmail.com>
2014-01-03 20:08:18 +0000

    Removed version from assembly reference so that the latest available will be resolved

---
+++ src/AssemblyContants.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public static class AssemblyConstants
+{
+    public const string Version = "1.0.1.0";
+}
+
--- src/CodedUI.Nuget/BuildPackage.bat
+++ src/CodedUI.Nuget/BuildPackage.bat
@@ -1,3 +1,3 @@
-copy ..\CodedUI.jQueryExtensions\bin\release\CodedUI.jQueryExtensions.dll content\lib\net45\ /Y
-copy ..\CodedUI.jQueryExtensions\bin\release\CodedUI.jQueryExtensions.XML content\lib\net45\ /Y
+copy ..\CodedUI.jQueryExtensions\bin\release\CodedUI.jQueryExtensions.dll content\lib\net40\ /Y
+copy ..\CodedUI.jQueryExtensions\bin\release\CodedUI.jQueryExtensions.XML content\lib\net40\ /Y
 NuGet Pack CodedUI.jQueryExtensions.nuspec
