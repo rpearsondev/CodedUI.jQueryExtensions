b02a550131845827fb90f437bd629baba6ceb460
rpearsondev <rpearsondev@gmail.com>
2014-01-27 21:33:19 +0000

    Updated packaing script. It now builds the solution and packages the nuget in the temp directory. It also clears up after itsself.

--- src/CodedUI.Nuget/BuildPackage.bat
+++ src/CodedUI.Nuget/BuildPackage.bat
@@ -1,3 +1,1 @@
-copy ..\CodedUI.jQueryExtensions\bin\release\CodedUI.jQueryExtensions.dll content\lib\net40\ /Y
-copy ..\CodedUI.jQueryExtensions\bin\release\CodedUI.jQueryExtensions.XML content\lib\net40\ /Y
-NuGet Pack CodedUI.jQueryExtensions.nuspec
+powershell .\BuildPackage.ps1
