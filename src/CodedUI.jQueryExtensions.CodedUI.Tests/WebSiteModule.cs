9434ee6a142bb6f5fd334620b3fbc1b48d0a79b0
rpearsondev <rpearsondev@gmail.com>
2014-05-28 22:49:03 +0100

    Added module file to integration tests project to facilitiate the running of the tests using nancy within the test project.

---
+++ src/CodedUI.jQueryExtensions.CodedUI.Tests/WebSiteModule.cs
@@ -0,0 +1,27 @@
+﻿using CodedUI.jQueryExtensions.Test.Core;
+using Nancy;
+
+namespace CodedUIjQuery.jQueryExtensions.CodedUI.Tests
+{
+    public class WebsiteModule : NancyModule
+    {
+        public WebsiteModule()
+        {
+            Get["/"] = _ => { return View["viewWebsiteRoot", new {}]; };
+
+            Get[Constants.Pages.SelectorTestPath] = _ => { return View["viewSelectorTests", new {}]; };
+
+            Get[Constants.Pages.ExistTestPath] = _ => { return View["viewExistsTests", new { }]; };
+
+            Get[Constants.Pages.WaitForExistsTestPath] = _ => { return View["viewWaitForExistsTests", new { }]; };
+
+            Get[Constants.Pages.ExistsInjectTests] = _ => { return View["viewJQueryExistsInjectTests", new { }]; };
+
+            Get[Constants.Pages.NotExistsInjectTests] = _ => { return View["viewJQueryNotExistsInjectTests", new { }]; };
+
+            Get[Constants.Pages.WaitForNotExistsTests] = _ => { return View["viewWaitForNotExistsTests", new { }]; };
+
+            Get[Constants.Pages.MethodsPage] = _ => { return View["viewMethodsTests", new { }]; };
+        }
+    }
+}
