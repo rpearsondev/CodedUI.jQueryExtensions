f2ce247f0a9d7244137d3abf690309851cf65e4c
rpearson <rpearsondev@gmail.com>
2014-10-02 16:49:15 +0100

    Removed website host as it is now obsolete.

--- src/CodedUI.jQueryExtensions.WebsiteHost/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CodedUI.jQueryExtensions.WebsiteHost")]
-[assembly: Guid("71ac183a-bf78-434a-b81a-c214d73b39bd")]
